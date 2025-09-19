88b68aa6fd366290889e1f098afe459d861f4797
jmarinrz <juan.marinr1@udea.edu.co>
2025-09-18 21:59:28 -0500

    Logica de clase temperatureSensor
    La clase temperatureSensor contiene la logica para generar temperaturas al azar cada segundo mediante un temporizador que actualiza el sistema cada segundo, y analiza si la temperatura pasa los umbrales de alerta, de ser asi invoca el evento para el caso necesario.

--- SistemaDeMonitoreo/temperatureSensor.cs
+++ SistemaDeMonitoreo/temperatureSensor.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Timers;
 
 public class temperatureSensor
 {
-	public Class1()
-	{
-	}
+	private double currentTemperature; //Variable donde se va a almacenar la temperatura actual
+    private Random random = new Random();//Variable para generar numeros aleatorios
+    private Timer timer;//Variable para controlar el tiempo
+
+    public temperatureSensor()
+    {
+        timer = new Timer(1000);//Se inicializa el timer para que se ejecute cada segundo
+        timer.Elapsed += (s,e) => generateTemperature();//Se llama a la funcion generateTemperature cada vez que se ejecuta el timer
+        timer.Start();//Se inicia el timer
+    }
+    public event EventHandler HighTemperatureReached;
+    public event EventHandler LowTemperatureReached;
+
+    public void GenerateTemperature()
+        {
+        currentTemperature = random.Next(20, 100);//Se genera un numero aleatorio entre 20 y 100
+        if(currentTemperature > 75)
+        {
+            HighTemperatureReached?.Invoke(this, EventArgs.Empty);//Si la temperatura es mayor a 75 se lanza el evento de alta temperatura
+        }
+        else if(currentTemperature < 30)
+        {
+            LowTemperatureReached?.Invoke(this, EventArgs.Empty);//Si la temperatura es menor a 30 se lanza el evento de baja temperatura
+        }
+    }
+    
+    
 }
